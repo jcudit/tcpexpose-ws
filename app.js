1ba465f29b3e7da853e32cba88f8322b6f65a6ce
Jeremy Udit <jcudit@gmail.com>
2016-12-22 21:05:30 -0800

    Proxy TCP state of a WebSocket client to that Websocket client

--- app.js
+++ app.js
@@ -7,8 +7,168 @@ var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
+var net = require('net');
 
 var app = express();
+var expressWs = require('express-ws')(app);
+
+const sep = ' ';
+
+// Tracks established WebSocket connections by mapping a client (IP, port)
+// pairing to a `ws` object
+var wsDict = {}
+
+// Prepare Unix socket that exposes Kernel socket stats
+const traceClient = net.connect({
+    // TODO: Remove hard-coding
+    path: '/var/run/tcpexpose.sock'
+});
+traceClient.setEncoding();
+
+// Remember listening socket of this server to allow access when tearing down
+// a WebSocket connection.  This is necessary because the information is gone
+// in the OS by the time that it is queried by the application.
+var local_ip, local_port;
+
+// The Unix socket writes TCP state data to this process in response to a
+// `flush` command (see `pollSocket`).  State information is then parsed here
+// and written out to the appropriate WebSocket client.  The client is matched
+// using the socket address points and the JSON-encoded data is relayed as-is.
+traceClient.on('data', function(msg){
+    msgs = msg.split('\n');
+
+    // Batch directional messages into single update for a WebSocket client
+    var batches = {}
+    msgs.forEach(function(message){
+        // Discard empty messages
+        if (message.length == 0) {
+            return;
+        }
+
+        try {
+            m = JSON.parse(message);
+        } catch (err) {
+            // Discard unparsable messages
+            console.log(err);
+            return;
+        }
+
+        // Build dictionary key as TCP socket quartet
+        k = m['saddr'] + sep + m['daddr'] + sep + m['sport'] + sep + m['dport'];
+
+        if (k in batches) {
+            // Append message to existing collection
+            batches[k].push(m)
+        } else if (reverseClientID(k) in batches) {
+            // Append message to existing matching collection
+            batches[reverseClientID(k)].push(m)
+        } else {
+            // Create new collection
+            batches[k] = [m]
+        }
+
+    });
+
+    // Write batched traces out to matching WebSocket client
+    for (var batch in batches) {
+        if (batch in wsDict) {
+            try {
+                wsDict[batch].send(JSON.stringify(batches[batch]));
+            } catch (err) {
+                // Remove tracked ws connection when WebSocket connection closes
+                unregisterClient(batch);
+                console.log(err);
+            }
+        }
+    }
+})
+
+
+// Returns the local and remote IP and port associated with a connected client
+function getClientID(req) {
+    var remote_ip, remote_port;
+
+    if (local_ip == undefined) {
+        local_ip = req.connection.localAddress;
+    }
+    if (local_port == undefined) {
+        local_port = req.connection.localPort;
+    }
+
+    if (req.header('X-Real-IP') && req.header('X-Real-Port')) {
+        // Proxy case
+        remote_ip = req.header('X-Real-IP');
+        remote_port = req.header('X-Real-Port');
+    } else {
+        // Direct case
+        remote_ip = req.connection.remoteAddress;
+        remote_port = req.connection.remotePort;
+    }
+    result = local_ip + sep + remote_ip + sep + local_port + sep + remote_port;
+    return result;
+}
+
+// Returns the remote and local IP and port associated with a connected client
+function reverseClientID(id) {
+    var parts = id.split(sep);
+    return parts[1] + sep + parts[0] + sep + parts[3] + sep + parts[2];
+}
+
+// Poll Unix socket recurringly by requesting TCP stats associated with a
+// target client's IP and source port.
+function pollSocket(id) {
+    try {
+        traceClient.write(id);
+    } catch (err) {
+        // TODO: Define behaviour for broken connection to Unix socket
+        console.log(err);
+    }
+
+    // Schedule next poll
+    if (id in wsDict) {
+        setTimeout(pollSocket, 3000, id);
+    }
+}
+
+// Track connected WebSocket clients so that trace responses can be relayed
+// asynchronously to the client after a polling request is made. Note that
+// TCP connections are tracked separately for each direction.
+function registerClient(id, ws) {
+    wsDict[id] = ws;
+    wsDict[reverseClientID(id)] = ws;
+}
+
+// Stop tracking connected WebSocket client. Note that this action removes
+// two entries as TCP connections are tracked separately for each direction.
+function unregisterClient(id) {
+    delete wsDict[id];
+    delete wsDict[reverseClientID(id)];
+}
+
+// When receiving the first message from a connected WebSocket client,
+// resgister the client and initiate polling the trace Unix socket for it's
+// TCP state information.
+//
+// When a client disconnects, unregister it to signal the end of polling for
+// that TCP connection.
+app.ws('/', function(ws, req) {
+    ws.on('message', function(msg) {
+        var id = getClientID(req);
+        if (id in wsDict) {
+            // Ignore msg if already polling for client
+            return
+        } else {
+            registerClient(id, ws);
+            pollSocket(id);
+        }
+    });
+
+    ws.on('close', function() {
+        var id = getClientID(req);
+        unregisterClient(id);
+    });
+});
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -44,5 +204,9 @@ app.use(function(err, req, res, next) {
     res.status(err.status || 500);
     res.render('error');
 });
+
+app.listen(3000, function() {
+    console.log('Example app listening on port 3000!')
+})
 
 module.exports = app;
